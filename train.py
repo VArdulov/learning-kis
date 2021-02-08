8231e81f62ce33ba271e20937a3f1006d3d48eb2
Victor Ardulov <ardulov@usc.edu>
2021-02-07 20:02:33 -0800

    updated to include cli argument for learning rate and to support GPU training

--- train.py
+++ train.py
@@ -15,6 +15,7 @@ import time
 
 from lkis import TimeSeriesBatchMaker, KoopmanInvariantSubspaceLearner
 from losses import combined_loss
+from torch import device
 from torch.optim import SGD
 
 # -- Parse arguments
@@ -29,25 +30,36 @@ parser.add_argument("--intermediate-observable", "-i", type=int, default=-1, hel
 parser.add_argument("--save-model", "-m", type=str, default="./", help="where to save you model")
 parser.add_argument("--save-training-plot", "-p", type=str, default="./", help="where to save plotting")
 parser.add_argument("--max-lag", "-l", type=int, default=-1, help="maximum_lag")
-parser.add_argument("--state-space", "-s", type=int, default=1)
+parser.add_argument("--state-space", "-s", type=int, default=1, help="dimensionality of the underlying state space")
 parser.add_argument("--alpha", "-a", type=float, default=1.0, help="value to score the reconstruction loss by")
+parser.add_argument("--learning-rate", "-r", type=float, default=0.001, help="Optimizer learning rate")
+
 if __name__ == "__main__":
+    # grab the command line arguments
     cli_args = parser.parse_args()
+
+    # find and load the training data
     data_path = cli_args.data_path
     print(f"Loading training data from {data_path}")
     data_train = np.load(data_path)
     if len(data_train.shape) == 1:
         data_train = data_train.reshape(-1, 1)
     print(f"Loaded a dataset with dimension: {data_train.shape}")
+
+    # process the delay either set by the user or is set to one 10th of the data
     delay = cli_args.max_lag if cli_args.max_lag > 0 else (data_train.shape[0] // 10)
+
+    # based on the number of batches, delay, and size of the data compute the samples per batch
     samples_per_batch = (data_train.shape[0] - delay) // cli_args.num_batches
 
+    # construct the data preparer
     batch_iterator = TimeSeriesBatchMaker(
         y=data_train,
         batch_size=samples_per_batch,
         max_lag=delay
     )
 
+    # construct the end-to-end model
     lkis = KoopmanInvariantSubspaceLearner(
         observable_dim=data_train.shape[1],
         latent_dim=cli_args.state_space,
@@ -55,12 +67,21 @@ if __name__ == "__main__":
         delay=delay
     )
 
-    optimizer = SGD(lkis.parameters(), lr=0.001)
+    if cli_args.gpu:
+        device = device("cuda")
+
+    # initialize the optimizer
+    optimizer = SGD(lkis.parameters(), lr=cli_args.learning_rate)
     losses = []
     for epoch in range(cli_args.epochs):
         loss = 0
         for b in range(cli_args.num_batches):
+            optimizer.zero_grad()
             time_delayed_ys, y_true = next(batch_iterator)
+
+            if cli_args.gpu:
+                time_delayed_ys.to(device)
+                y_true.to(device)
 
             g_pred, y_pred = lkis(time_delayed_ys)
             g_0 = g_pred[:-1]
